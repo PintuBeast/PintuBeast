74823c4cd4dfc6db9f03d62b96599406f6350ddb
pintubeast <abhishek.jk1992@gmail.com>
2021-05-17 15:15:57 +0530

    first

---
+++ Mooplay/trial/mooplay/App.js
@@ -0,0 +1,29 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import Home from "./src/screens/Home";
+import {
+  SafeAreaView,
+  StatusBar
+} from 'react-native';
+
+
+
+const App: () => Node = () => {
+  
+  return (
+    <SafeAreaView >
+    <Home/>
+    </SafeAreaView>
+
+  );
+};
+
+
+export default App;
